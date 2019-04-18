de7bf676ba3b0edaa5210801fce839fe076cf7fd
Milos <milosrancic94@gmail.com>
2019-04-18 20:46:08 +0200

    added progress bar js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let progress = new ProgressBar.Circle("#progress", {
+  strokeWidth: 6,
+  easing: "easeInOut",
+  // duration: 1400,
+  // color: "#FFEA82",
+  // trailColor: "#eee",
+  // trailWidth: 1,
+  // svgStyle: null,
+  text: {
+    value: 80
+  }
+});
+
+progress.animate(1.0); // Number from 0.0 to 1.0
