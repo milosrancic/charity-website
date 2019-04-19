1f6680ef16c715a575750a2dfa6d498e5421c661
Milos <milosrancic94@gmail.com>
2019-04-19 23:08:03 +0200

    commented out whole progress bar code

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-let progress = new ProgressBar.Circle("#progress", {
-  strokeWidth: 6,
-  easing: "easeInOut",
-  // duration: 1400,
-  // color: "#FFEA82",
-  // trailColor: "#eee",
-  // trailWidth: 1,
-  // svgStyle: null,
-  text: {
-    value: 80
-  }
-});
+// let progress = new ProgressBar.Circle("#progress", {
+//   strokeWidth: 6,
+//   easing: "easeInOut",
+// duration: 1400,
+// color: "#FFEA82",
+// trailColor: "#eee",
+// trailWidth: 1,
+// svgStyle: null,
+//   text: {
+//     value: 80
+//   }
+// });
 
-progress.animate(1.0); // Number from 0.0 to 1.0
+// progress.animate(1.0);
+// Number from 0.0 to 1.0
